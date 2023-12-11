451eaeef59cb945c564201c988c485edf1cb9b54
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2023-12-11 12:20:11 -0300

    Refactor ensureAuthenticated middleware

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -21,15 +21,15 @@ export async function ensureAuthenticated(
 
   const [, token] = authHeader.split(" ");
 
-  try {
-    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
-
-    request.user = {
-      id: user_id,
-    };
-
-    next();
-  } catch {
+  const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
+  
+  if (!user_id) {
     throw new UnauthorizedError("Invalid token!");
   }
+
+  request.user = {
+    id: user_id,
+  };
+
+  next();
 }
